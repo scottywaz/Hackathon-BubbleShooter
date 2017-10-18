9379ead5d11b467f9f26b290a8f0333cddcb9cd2
Scott Wszalek <swszalek@gsngames.com>
2017-10-18 15:19:06 -0400

    Removed references to the parallax background

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -10,7 +10,6 @@ public class UIManager : MonoBehaviour
 
     public Text _centerText;
     public Text _score;
-    public Parallax _background;
 
     // Use this for initialization
     void Start()
@@ -27,14 +26,12 @@ public class UIManager : MonoBehaviour
     {
         _centerText.gameObject.SetActive(true);
         _centerText.text = "Game Over";
-        _background.StopMode();
     }
 
     public void DisplayWin()
     {
         _centerText.gameObject.SetActive(true);
         _centerText.text = "Win";
-        _background.StopMode();
     }
 
     public void UpdateScore(int score)
@@ -49,12 +46,10 @@ public class UIManager : MonoBehaviour
 
     public void TurnOnRedAlert()
     {
-        _background.RedAlert();
     }
 
     public void NormalMode()
     {
-        _background.NormalMode();
     }
 
 }
